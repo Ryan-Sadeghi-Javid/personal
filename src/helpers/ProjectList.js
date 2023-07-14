36f7fa35310e384fc2de4067aa4444137b839a42
Ryan-Sadeghi-Javid <javidryan46@gmail.com>
2023-07-14 00:00:24 -0400

    Finished project cards

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,29 @@
+import java_physics_engine_image from '../assets/java_physics_engine_image.PNG'
+import waterpunk_image from '../assets/waterpunk_image.PNG'
+import healic_image from '../assets/healic_image.png'
+import arduinkler_image from '../assets/arduinkler_image.jpg'
+import sudoku_solver_image from '../assets/sudoku_solver_image.PNG'
+
+
+export const ProjectList = [
+    {
+        name: "Java Physics Engine",
+        image: java_physics_engine_image
+    },
+    {
+        name: "Waterpunk: 4D Video Game",
+        image: waterpunk_image
+    },
+    {
+        name: "Healic: Match and chat platform",
+        image: healic_image
+    },
+    {
+        name: "Arduinkler: Remote watering system",
+        image: arduinkler_image
+    },
+    {
+        name: "Sudoku Solver",
+        image: sudoku_solver_image
+    }
+]
